2b2165847e7864d65bae90eab8c6896592e439b3
MB <vthokie.manoj@gmail.com>
2026-05-11 21:30:49 -0600

    Initial Commit

--- FirstGrokProject/FirstGrokProjectApp.swift
+++ FirstGrokProject/FirstGrokProjectApp.swift
@@ -5,8 +5,16 @@ import SwiftData
 struct FirstGrokProjectApp: App {
     var body: some Scene {
         WindowGroup {
+<<<<<<< HEAD
             ContentView()
         }
         .modelContainer(for: SavedTournament.self)
     }
 }
+=======
+            HomeView()
+        }
+        .modelContainer(for: SavedTournament.self)
+    }
+}
+>>>>>>> 36bef8d (Initial Commit)
--- FirstGrokProject/Models/TournamentModels.swift
+++ FirstGrokProject/Models/TournamentModels.swift
@@ -13,7 +13,12 @@ class SavedTournament {
     var blindLevels: [BlindLevel]
     var colorUpLevels: [Int]
     
+<<<<<<< HEAD
     init(name: String, players: Int, totalMinutes: Int, initialStack: Int, startingSmallBlind: Int, blindLevels: [BlindLevel], colorUpLevels: [Int]) {
+=======
+    init(name: String, players: Int, totalMinutes: Int, initialStack: Int,
+         startingSmallBlind: Int, blindLevels: [BlindLevel], colorUpLevels: [Int]) {
+>>>>>>> 36bef8d (Initial Commit)
         self.id = UUID()
         self.name = name
         self.dateCreated = Date()
@@ -40,4 +45,8 @@ struct ChipDenomination: Identifiable, Codable, Hashable {
     var id = UUID()
     var value: Int
     var count: Int
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 36bef8d (Initial Commit)
