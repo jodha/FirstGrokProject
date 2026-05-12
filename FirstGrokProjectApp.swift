9434c9655cb9240fdf7b29dc82982dc5ea8df637
jodha <vthokie.manoj@gmail.com>
2026-05-11 21:50:40 -0600

    Initial commit: Set up main app with SwiftData

---
+++ FirstGrokProjectApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct FirstGrokProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+        .modelContainer(for: SavedTournament.self)
+    }
+}
